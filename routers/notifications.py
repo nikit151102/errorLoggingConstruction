88fdd0b893e17bc03cee64b2f223535b403dbd81
nikit151102 <nikit15110295@gmail.com>
2025-01-13 01:50:49 +0700

    добавлена почта и пояс Москвы

--- routers/notifications.py
+++ routers/notifications.py
@@ -1,18 +1,19 @@
 from fastapi import APIRouter
 from schemas.notifications import UserActionDto
 from utils.telegram import send_error_to_telegram
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 
 router = APIRouter()
 
 @router.post("/userActionInfo")
 async def log_user_action_to_telegram(user_action: UserActionDto):
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M")
+    current_time = datetime.now(timezone(timedelta(hours=3))).strftime("%Y-%m-%d %H:%M")
 
     user_message = (
         f"<b>Информация о пользователе:</b>\n"
         f"<b>ID пользователя:</b> {user_action.user_id}\n"
         f"<b>ФИО:</b> {user_action.user_full_name}\n"
+        f"<b>Почта:</b> {user_action.email}\n"
         f"<b>Действие:</b> {user_action.action}\n"
         f"<b>Время:</b> {current_time}\n"
     )
--- schemas/notifications.py
+++ schemas/notifications.py
@@ -3,4 +3,5 @@ from pydantic import BaseModel
 class UserActionDto(BaseModel):
     user_id: str
     user_full_name: str
+    email: str
     action: str
